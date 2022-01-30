982ea6e1ce0d962ab1d5e7ae34658fe45094f967
mhfuad <mhfuad2@gmail.com>
2022-01-30 14:25:55 +0600

    home route added

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 import express from "express";
+import bodyParser from 'body-parser';
+
+const app = express();
+
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res)=>{
+    console.log('[test}!');
+
+    res.send("hello from Home page");
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Server running on port: http://localhost:${PORT}`)
+})
